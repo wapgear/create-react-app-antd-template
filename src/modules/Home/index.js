581c79eed3f0ce281f27e8ed27141ddb9e21ed19
Anton Izmailov <wapgear@Antons-iMac.local>
2016-12-19 18:54:33 +0300

    home page section

--- src/modules/Home/index.js
+++ src/modules/Home/index.js
@@ -1,27 +1,36 @@
-import React from 'react'
+import React    from 'react'
+import {Button} from 'antd'
+
+import './styles.css'
 
 export default class HomeModule extends React.Component {
 
   render() {
     return (
-        <div>
+        <div className="homeModule">
           <section className="page banner-wrapper">
               <div id="banner">
                   <div className="banner-text-wrapper">
                       <h2 className="" style={{opacity: 1, visibility: 'visible', transform: 'translateX(0px)'}}>
-                        ANT <p>DESIGN</p>
+                        Anton <p>Izmailov</p>
                       </h2>
                       <p className="" style={{opacity: 1, visibility: 'visible', transform: 'translateX(0px)'}}>
-                          <span>One Design Language for UI</span>
+                          <span><span className="specialText">ReactJS</span> Developer</span>
                       </p>
                       <span className="line" style={{opacity: 1, visibility: 'visible', transform: 'translateX(0px)'}}></span>
                       <div className="start-button clearfix" style={{opacity: 1, visibility: 'visible', transform: 'translateX(0px)'}}>
-                          <a href="/docs/spec/introduce">
-                              <span>Introduce</span>
-                          </a>
-                          <a href="/docs/react/introduce">
-                              <span>Get Started</span>
-                          </a>
+                        <Button
+                          size="large"
+                          type="ghost"
+                        >
+                          About me
+                        </Button>
+                        <Button
+                          size="large"
+                          type="ghost"
+                        >
+                          My blog
+                        </Button>
                       </div>
                   </div>
                   <i className="anticon anticon-down down"></i>
